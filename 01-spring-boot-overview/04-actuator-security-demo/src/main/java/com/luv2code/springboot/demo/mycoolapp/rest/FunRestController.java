af7bb73afc3b6a44c86e482763f387df2c741ac1
William <wifryo@gmail.com>
2023-04-12 20:15:23 +0200

    Add actuator & configurations

--- 01-spring-boot-overview/04-actuator-security-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/04-actuator-security-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,23 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${chode.name}")
+    private String chodeName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoint for new info
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Chode: " + chodeName + ", team name: " + teamName;
+    }
 
     // expose "/" that returns "Hello World"
     @GetMapping("/")
@@ -25,3 +38,4 @@ public class FunRestController {
         return "Hee bogger!";
     }
 }
+
