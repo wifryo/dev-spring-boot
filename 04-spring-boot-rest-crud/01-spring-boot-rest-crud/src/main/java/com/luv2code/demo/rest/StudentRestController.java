7a4b13e753339e297c09a656d886e57af2c540cd
William <wifryo@gmail.com>
2023-04-19 21:40:12 +0200

    Implement exception handling

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -1,9 +1,10 @@
 package com.luv2code.demo.rest;
 
 import com.luv2code.demo.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,15 +12,48 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    private List<Student> theStudents;
+
+    // define @PostConstruct to load the student data - only once
+
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
+        theStudents.add(new Student("Glab", "Arseley"));
+        theStudents.add(new Student("Nibbs", "Crendle"));
+        theStudents.add(new Student("Bube", "Snib"));
+    }
     // define endpoint for "/students" - return a list of students
 
     @GetMapping("/students")
     public List<Student> getStudents() {
-        List<Student> theStudents = new ArrayList<>();
-
-        theStudents.add(new Student("Glab", "Arseley"));
-        theStudents.add(new Student("Nibbs", "Crendle"));
-        theStudents.add(new Student("Bube", "Snib"));
         return theStudents;
+    }
+
+    // define endpoint or "/students/{studentId}" - return student at index
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        // check the studentId against list size
+        if ((studentId <= theStudents.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student ID not found - " + studentId);
+        }
+        // index into the list
+        return theStudents.get(studentId);
+    }
+
+    // Add an exception handler using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
